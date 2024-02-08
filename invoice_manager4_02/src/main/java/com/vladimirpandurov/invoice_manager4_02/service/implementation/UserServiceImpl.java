0c9894627249f057def5c4eee27085db50bea58e
panvla <pandvlado@gmail.com>
2024-02-08 19:33:36 +0100

    service and dto mapper done

---
+++ invoice_manager4_02/src/main/java/com/vladimirpandurov/invoice_manager4_02/service/implementation/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.vladimirpandurov.invoice_manager4_02.service.implementation;
+
+import com.vladimirpandurov.invoice_manager4_02.domain.User;
+import com.vladimirpandurov.invoice_manager4_02.dto.UserDTO;
+import com.vladimirpandurov.invoice_manager4_02.dtomapper.UserDTOMapper;
+import com.vladimirpandurov.invoice_manager4_02.repository.UserRepository;
+import com.vladimirpandurov.invoice_manager4_02.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository<User> userRepository;
+
+    @Override
+    public UserDTO createUser(User user) {
+        return UserDTOMapper.fromUser(this.userRepository.create(user));
+    }
+}
